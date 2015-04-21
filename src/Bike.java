06d3297f7d79ef652722a2067ba678be3cb32a04
Carl Bergman <bergman.carl@gmail.com>
2015-04-21 16:21:23 +0200

    Added class comments

--- src/Bike.java
+++ src/Bike.java
@@ -1,18 +1,35 @@
-
+/**
+ * Class handling bikes.
+ * @author carlbergman
+ *
+ */
 public class Bike {
 
 	private String id;
 	private User user;
-	
+
+	/**
+	 * Bike constructor
+	 * @param id The barcode id
+	 * @param user The user (owner)
+	 */
 	public Bike (String id, User user) {
 		this.id = id;
 		this.user = user;
 	}
 	
+	/**
+	 * Get the user
+	 * @return User the user
+	 */
 	public User getUser () {
 		return user;
 	}
 	
+	/**
+	 * Get barcode id
+	 * @return String the barcode id
+	 */
 	public String getId () {
 		return id;
 	}
--- src/User.java
+++ src/User.java
@@ -1,24 +1,46 @@
-
+/**
+ * Class handling users. 
+ * @author carlbergman
+ *
+ */
 public class User {
 
-	private int pin;
-	private String name;
-	private String pn;
+	private int pin;		// Person pin code
+	private String name;	// Person name
+	private String pn;		// Personal id number
 	
+	/**
+	 * User constructor
+	 * @param pin User pin code
+	 * @param name User name
+	 * @param pn User personal id number
+	 */
 	public User (int pin, String name, String pn) {
 		this.pin = pin;
 		this.name = name;
 		this.pn = pn;		
 	}
 	
+	/**
+	 * Get pin code
+	 * @return int the pin code
+	 */
 	public int getPin () {
 		return pin;
 	}
 	
+	/**
+	 * Get name
+	 * @return String the name
+	 */
 	public String getName () {
 		return name;
 	}
 	
+	/**
+	 * Get personal id number
+	 * @return String the personal id
+	 */
 	public String getPn () {
 		return pn;
 	}
