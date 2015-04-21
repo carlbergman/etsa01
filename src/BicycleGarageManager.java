2a35ab0c899f25f4d519e4637312eee1915efe3c
Carl Bergman <bergman.carl@gmail.com>
2015-04-21 15:54:19 +0200

    Added attributes

--- src/BicycleGarageManager.java
+++ src/BicycleGarageManager.java
@@ -1,20 +1,28 @@
-
 public class BicycleGarageManager {
 
+	private BarcodePrinter printer;
+	private ElectronicLock entryLock;
+	private ElectronicLock exitLock;
+	private PinCodeTerminal terminal;
+	
 	public void registerHardwareDrivers(BarcodePrinter printer,
 			ElectronicLock entryLock, ElectronicLock exitLock,
 			PinCodeTerminal terminal) {
-		
+
+		this.printer = printer;
+		this.entryLock = entryLock;
+		this.exitLock = exitLock;
+		this.terminal = terminal;
 		
 	}
 
 	public void entryBarcode(String code) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void entryCharacter(char c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
