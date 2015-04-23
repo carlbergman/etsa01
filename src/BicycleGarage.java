33e3676f287a300b6408560f1129a7a44175588a
Anton Schölin <antonscholin@gmail.com>
2015-04-23 17:45:37 +0200

    merge

--- src/BicycleGarage.java
+++ src/BicycleGarage.java
@@ -15,10 +15,10 @@ public class BicycleGarage {
 	 */
 	public BicycleGarage() {
 		HashMap<String, Bike> bikes = (HashMap<String, Bike>) loadObjectFromFile("bikes.txt");
-		HashMap<Integer, User> users = (HashMap<Integer, User>) loadObjectFromFile("users.txt");
 		HashMap<String, User> users = (HashMap<String, User>) loadObjectFromFile("users.txt");
 
 		BicycleGarageManager manager = new BicycleGarageManager(users, bikes);
+
 		ElectronicLock entryLock = new ElectronicLockTestDriver("Entry lock");
 		ElectronicLock exitLock = new ElectronicLockTestDriver("Exit lock");
 		BarcodePrinter printer = new BarcodePrinterTestDriver();
