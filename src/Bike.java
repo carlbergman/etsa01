7988e784f6338c0d34f6e7b12f3c64e78e758665
Anton Schölin <antonscholin@gmail.com>
2015-05-22 11:46:41 +0200

    fixed bike equals

--- src/Bike.java
+++ src/Bike.java
@@ -2,6 +2,7 @@ import java.io.Serializable;
 
 /**
  * Class handling bikes.
+ * 
  * @author carlbergman
  *
  */
@@ -11,69 +12,90 @@ public class Bike implements Serializable {
 	 * For serializing and de-serializing. Do not change.
 	 */
 	private static final long serialVersionUID = -3497924117832596736L;
-	
+
 	/**
 	 * Attributes
 	 */
-	private String barcode;		// Bike identifier
-	private User user;			// Bike owner
-	private boolean status;		// Whether bike is registered as in garage
+	private String barcode; // Bike identifier
+	private User user; // Bike owner
+	private boolean status; // Whether bike is registered as in garage
 
 	/**
 	 * Bike constructor
-	 * @param barcode The barcode
-	 * @param user The user (owner)
+	 * 
+	 * @param barcode
+	 *            The barcode
+	 * @param user
+	 *            The user (owner)
 	 */
-	public Bike (String id, User user) {
+	public Bike(String id, User user) {
 		this.barcode = id;
 		this.user = user;
 		status = false;
 	}
-	
+
 	/**
 	 * Get the user
+	 * 
 	 * @return User the user
 	 */
-	public User getUser () {
+	public User getUser() {
 		return user;
 	}
-	
+
 	/**
 	 * Get barcode
+	 * 
 	 * @return String the barcode
 	 */
-	public String getId () {
+	public String getId() {
 		return barcode;
 	}
-	
+
 	/**
-	 * Get status (whether the system should be regarded as placed in the garage by the system.)
+	 * Get status (whether the system should be regarded as placed in the garage
+	 * by the system.)
+	 * 
 	 * @return true if the bike is in the garage, otherwise false
 	 */
-	public boolean getStatus () {
+	public boolean getStatus() {
 		return status;
 	}
-	
+
 	/**
 	 * Check in the bike.
 	 */
-	public void bikeIn(){
-		status=true;
+	public void bikeIn() {
+		status = true;
 	}
-	
+
 	/**
 	 * Check out the bike.
 	 */
-	public void bikeOut(){
-		status=false;
+	public void bikeOut() {
+		status = false;
 	}
-	
+
+	/**
+	 * Checks if this bike is equal to another bike
+	 * @param obj The object to compare to
+	 * @return true if equal, false otherwise
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof Bike) {
+			if (((Bike) obj).getId().equals(barcode)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	/**
 	 * Get a string representation of the class.
 	 * 
 	 * @return a string.
 	 */
-	public String toString(){
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Bar code: " + barcode);
 		sb.append("   ");
